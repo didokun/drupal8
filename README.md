9cb55186fdc97d697149b2c5567e28d6e3882a0c
Adil Echcharafi <charafi.adil@gmail.com>
2019-07-07 15:28:43 +0200

    Create README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# drupal8
+Run Drupal 8 with Composer + Docker-compose + Nginx + MariaDB + PHP7.3 
+
+[Tutorial in medium](https://medium.com/@didokun/run-drupal-8-with-composer-docker-compose-nginx-mariadb-php7-3-ca7d0cc71a99) 
--- src/web/index.php
+++ src/web/index.php
@@ -7,7 +7,6 @@
  * All Drupal code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
  */
-
 use Drupal\Core\DrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
 
